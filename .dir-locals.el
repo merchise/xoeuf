50a524dd21644edfde4bf58e1d894f1b2ecfaa82
Manuel Vázquez Acosta <mvaled@pm.me>
2024-01-25 17:17:44 +0100

    Update '.dir-locals.el'

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
-;;; Directory Local Variables
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-mode
-	(fill-column . 100)))
+((python-mode . ((projectile-project-compilation-cmd . "make test")
+                  (fill-column . 100))))
